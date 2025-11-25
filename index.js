426570bd91885b26beff6600130f4a663550bd7c
marwaneTRK <marwantraiki@gmail.com>
2025-11-26 00:29:32 +0100

    DOM and js function test

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+
+const generatorBtn = document.querySelector(".gen")
+const one = document.querySelector(".one")
+const two = document.querySelector(".two")
+
+function hope(){
+let result =""
+    for (let i = 0; i < characters.length; i++) {
+        result += Math.floor(Math.random())
+        one.textContent =   '  test'   
+    }
+}
