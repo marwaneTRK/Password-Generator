70e7e6342170bdc3681f9a7d1d2a9a4e863727c6
marwaneTRK <marwantraiki@gmail.com>
2025-11-26 22:25:21 +0100

    responsive with Matrix background

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "/"];
 
 
+
 const generatorBtn = document.querySelector(".gen")
 const result = document.querySelector(".result")
+
 let res = ""
-let state = false
+
 const size = (document.getElementById('lengthInput')) 
 const box = document.getElementById('box')
 
@@ -20,23 +22,78 @@ if (size.value > max) size.value = max
 
 // fitler 52 index
 function hope(){
-// state = !state;
+
 let validSize = parseInt(size.value)
 
-  state = !state;
-    if (validSize > 0 && state) {
+  
+    if (validSize > 0  && !box.checked) {
         for (let i = 0; i < validSize; i++) {
     res +=  characters[Math.floor(Math.random() * characters.length)]   
 }
-  state = !state;
-
-
 result.textContent = res
+
+}else if (validSize > 0  && box.checked ){
+        for (let i = 0; i < validSize; i++) {
+    res +=  characters[Math.floor(Math.random() * 52)] 
+
 }
-if (state === false){
+result.textContent = res
+
+}else if(validSize ===0){
+    res =""
+}
+
     res = " "
-}
+
 
 }
 
-console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+        var c = document.getElementById("c");
+        var ctx = c.getContext("2d");
+
+        c.height = window.innerHeight;
+        c.width = window.innerWidth;
+
+        var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+        matrix = matrix.split("");
+
+        var font_size = 10;
+        var columns = c.width/font_size; 
+        var drops = [];
+        
+        for(var x = 0; x < columns; x++)
+            drops[x] = 1; 
+
+        function draw()
+        {
+            
+            ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+            ctx.fillRect(0, 0, c.width, c.height);
+
+            ctx.fillStyle = "#10B981";
+            ctx.font = font_size + "px arial";
+            for(var i = 0; i < drops.length; i++)
+            {
+                var text = matrix[Math.floor(Math.random()*matrix.length)];
+                ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+                if(drops[i]*font_size > c.height && Math.random() > 0.975)
+                    drops[i] = 0;
+
+                drops[i]++;
+            }
+        }
+
+        setInterval(draw, 35);
