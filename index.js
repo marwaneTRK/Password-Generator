9255a05dda900a59e56db8fdc1bc1f660a1a41c6
marwaneTRK <marwantraiki@gmail.com>
2025-11-26 15:57:47 +0100

    logical app first succes

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", 
 "/"];
 
 
 const generatorBtn = document.querySelector(".gen")
-const one = document.querySelector(".one")
-const two = document.querySelector(".two")
+const result = document.querySelector(".result")
+let res = ""
+let state = false
 
 function hope(){
-let result =""
-    for (let i = 0; i < characters.length; i++) {
-        result += Math.floor(Math.random())
-        one.textContent =   '  test'   
-    }
+// state = !state;
+const size = parseInt(document.getElementById('lengthInput').value) 
+
+  state = !state;
+    if (size > 0 && state) {
+        for (let i = 0; i < size; i++) {
+    res +=  characters[Math.floor(Math.random() * characters.length)]   
 }
+  state = !state;
+
+
+result.textContent = res
+}
+if (state === false){
+    res = " "
+}
+
+}
+
+
+console.log(res);
